9da469ce1786bfd71327b9e820ddb79ff9c0b0f0
Roman Malitskyi <chebureck77@gmail.com>
2024-09-23 10:57:06 +0300

    update scrip

--- Assets/RpgAdventure/Scripts/Ui/MainMenu/SelectCharacterPage.cs
+++ Assets/RpgAdventure/Scripts/Ui/MainMenu/SelectCharacterPage.cs
@@ -10,16 +10,33 @@ namespace TandC.RpgAdventure.UI.MainMenu
     {
         public bool IsActive { get; private set; }
 
+        private GameObject _selfObject;
+
+        private CanvasGroup _canvasGroup;
+
+        private IUIService _uiService;
+        private DataService _dataService;
+
         public void Init(IUIService uiService, DataService dataService)
         {
+            _uiService = uiService;
+            _dataService = dataService;
+
+            InitializeAllVariables();
+
+            SetupButtons();
+
+            Hide();
         }
 
         public void Show()
         {
+            ChangeState(true);
         }
 
         public void Hide()
         {
+            ChangeState(false);
         }
 
         public void Update()
@@ -27,6 +44,30 @@ namespace TandC.RpgAdventure.UI.MainMenu
         }
 
         public void Dispose()
+        {
+            _selfObject = null;
+            _canvasGroup = null;
+
+            _uiService = null;
+        }
+
+        private void ChangeState(bool isOn)
+        {
+            IsActive = isOn;
+
+            _canvasGroup.alpha = isOn ? 1.0f : 0.0f;
+            _canvasGroup.interactable = isOn;
+            _canvasGroup.blocksRaycasts = isOn;
+        }
+
+        private void InitializeAllVariables()
+        {
+            _selfObject = _uiService.Canvas.transform.Find("Page_Settings").gameObject;
+
+            _canvasGroup = _selfObject.GetComponent<CanvasGroup>();
+        }
+
+        private void SetupButtons()
         {
         }
     }
